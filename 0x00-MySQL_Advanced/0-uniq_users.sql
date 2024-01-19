59f1c883613eff3bbc5a0861ded2b2b97dae253b
Omar Khaled <o.khaled6789@gmail.com>
2024-01-19 04:58:52 +0200

    added 1-6

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+--  a SQL script that creates a table users with these attributes:
+--	id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,18 @@
+-- creates a stored procedure AddBonus
+-- that adds a new correction for a student.
+DELIMITER $$
+CREATE PROCEDURE AddBonus(
+	IN user_id INTEGER,
+	IN project_name VARCHAR(255),
+	IN score FLOAT
+)
+BEGIN
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, project_id, score);
+END;
+$$
+DELIMITER ;
