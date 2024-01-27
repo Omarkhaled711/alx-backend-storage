b25672933fd3b182e347c344205f6dc37df3432c
Omar Khaled <o.khaled6789@gmail.com>
2024-01-27 05:00:08 +0200

    added 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+ a Python script that provides some stats about Nginx logs stored in MongoDB:
+"""
+import pymongo
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    log_nums = nginx_collection.count_documents({})
+    print(f'{log_nums} logs')
+
+    methods = ["GET", "POST", "PUT", "DELETE", "PATCH"]
+    print('Methods:')
+    for method in methods:
+        counted = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {counted}')
+
+    check_status = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{check_status} status check')
